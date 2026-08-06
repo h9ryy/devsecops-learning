b26cde038ac04843499a76aa96af6836b3e1cb92
w1zer <w1zer@example.com>
2026-08-06 14:53:00 +0300

    fix(srv_backup): исправил импорты, указал check=False

--- python-scripts/srv_backup/srv_backup.py
+++ python-scripts/srv_backup/srv_backup.py
@@ -1,11 +1,13 @@
 import argparse
 import os
-import sys
 import subprocess
+import sys
 import time
 
 parser = argparse.ArgumentParser(description="Configuration backup")
-parser.add_argument("-d", "--dir", type=str, required=True, help="Path to configuration")
+parser.add_argument(
+    "-d", "--dir", type=str, required=True, help="Path to configuration"
+)
 args = parser.parse_args()
 
 if not os.path.exists(args.dir):
@@ -15,14 +17,19 @@ if not os.path.exists(args.dir):
 try:
     backup_name = f"backup{int(time.time())}.tar.gz"
     command = ["tar", "-czf", backup_name, args.dir]
-    
-    sub_run = subprocess.run(command, capture_output=True, text=True, timeout=60)
+
+    sub_run = subprocess.run(
+        command, capture_output=True, text=True, timeout=60, check=False
+    )
     if sub_run.returncode == 0:
         print(f"[OK] Backup of '{args.dir}' created successfully as {backup_name}.")
         sys.exit(0)
     else:
-        print(f"[ERROR] Tar command failed with exit code {sub_run.returncode}. Details: {sub_run.stderr}",file=sys.stderr,)
+        print(
+            f"[ERROR] Tar command failed with exit code {sub_run.returncode}. Details: {sub_run.stderr}",
+            file=sys.stderr,
+        )
         sys.exit(1)
 except subprocess.TimeoutExpired:
-    print(f"[ERROR] Backup process timed out after 60 seconds.", file=sys.stderr)
+    print("[ERROR] Backup process timed out after 60 seconds.", file=sys.stderr)
     sys.exit(1)
